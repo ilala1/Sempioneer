85aa902fca40ad710e54416ddc0f3cf836b7e3c3
Imran Lala <imran@enigma-marketing.co.uk>
2020-07-08 15:50:31 +0100

    commented out all code of gsc.js

--- client/lib/gsc.js
+++ client/lib/gsc.js
@@ -1,18 +1,27 @@
 // var schedule = require('node-schedule');
+// import { apiGet, apiPut, apiPost } from './api';
 
+
+// const oneUser = apiGet({}, '/oneUser', {103456017882470757103});
+
+// console.log(oneUser)
 
 // export const test = (ctx) => {
 //     console.log('hellooo');
-//     var j = schedule.scheduleJob('0 0 * * *', function(){
-//         console.log('The answer to life, the universe, and everything!');
+//     var j = schedule.scheduleJob('0 1 * * *', function(){
+//             // const oneUser = apiGet({}, '/oneUser', {103456017882470757103});
+//             // console.log(oneUser)
+
+//             console.log('The answer to life, the universe, and everything is pizza!');
 //       });
 // }
 
-export const signOut = () => {
-  console.log('signing out')
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-}
+// export const signOut = () => {
+//   console.log('signing out')
+//   var auth2 = gapi.auth2.getAuthInstance();
+//   auth2.signOut().then(function () {
+//     console.log('User signed out.');
+//   });
+// }
 
+// export default test;
